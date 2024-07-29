f0349ad1b9ac2208f30ad656313f238249a5f717
unknown <shokirjonovokifxon46@gmail.com>
2024-07-29 11:40:17 +0500

    vazifa

--- src/components/dtails/index.jsx
+++ src/components/dtails/index.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import '../dtails/index.css'
 
 const Dtails = () => {
+  const navigate = useNavigate()
   const { id } = useParams();
   const [data, setData] = useState(null);
 
@@ -35,6 +36,9 @@ const Dtails = () => {
   if (!data) {
     return <div></div>
   }
+  function handleback(){
+    navigate('/')
+  }
 
   return (
     <div className='blowing'>
@@ -45,6 +49,13 @@ const Dtails = () => {
         <h1>{data.name}</h1>
         <h3>New Price: {data.newPrice}</h3>
         <h3>Old Price: {data.oldPrice}</h3>
+        {/* <button onClick={handleback}>Back</button> */}
+        <a class="fancy" onClick={handleback} href="#">
+  <span class="top-key"></span>
+  <span class="text">Back</span>
+  <span class="bottom-key-1"></span>
+  <span class="bottom-key-2"></span>
+</a>
       </div>
     </div>
   );
